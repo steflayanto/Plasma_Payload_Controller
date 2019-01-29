655f985383570fe0c5fc7aa3fb49f0d1665c13b5
Stefan Layanto <stef.layanto@gmail.com>
2019-01-29 12:39:49 -0800

    initial commit

---
+++ Master_Controller/Master_Controller.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include "Header.h"
+
+void setup() {
+  Serial.begin(116200);
+  initSensors();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+void initSensors() {
+  //put all sensor initialization code here
+}
