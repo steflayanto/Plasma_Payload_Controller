9811232b181a901f1c09cb4280d40314135c3d74
aiyabunni <lchau7@uw.edu>
2019-02-26 14:16:25 -0800

    added packaging for BME

---
+++ packagingBME/packagingBME.ino
@@ -0,0 +1,48 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+
+void initBME() {
+  //in setup
+  Serial.begin(9600);
+  Serial.println(F("BME280 test"));
+
+  bool status;
+
+  status = bme.begin();
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+}
+
+float BMEtemp() {
+  float val = bme.readTemperature();
+  return val;
+}
+
+float BMEpres() {
+  float val = bme.readPressure() / 100.0F;
+  return val;
+}
+
+float BMEalt() {
+  float val = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  return val;
+}
+
+float BMEhum() {
+  float val = bme.readHumidity();
+  return val;
+}
