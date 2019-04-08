cd46631ee76d2d52356cbeb63627302225e447a3
Evan Stanton <emstanto@uw.edu>
2019-04-08 12:49:59 -0700

    Simple ADC testing program

---
+++ Flux_Sensor_Test/basic_ADC_test/basic_ADC_test.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+#include <SparkFunTMP102.h>
+
+Adafruit_ADS1115 adc1(0x48);
+Adafruit_ADS1115 adc2(0x49);
+TMP102 tempSens(0x48);
+void setup() {
+  adc1.begin();
+  tempSens.begin();
+  adc2.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  int16_t f1, f2;
+  f1 = adc1.readADC_Differential_0_1();
+  f2 = adc2.readADC_Differential_0_1();
+  float t = tempSens.readTempF();
+//  Serial.print("t: ");
+//  Serial.println(t);
+  Serial.print("f1: ");
+  Serial.println(f1);
+  Serial.println(f2);
+  delay(400);
+}
