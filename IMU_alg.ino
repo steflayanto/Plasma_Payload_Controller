128d92e93bebc8630eaeccc733fdb6705a0aae55
adnorgaard <48578545+adnorgaard@users.noreply.github.com>
2019-05-17 17:57:43 -0700

    Add files via upload

--- IMU_alg.ino
+++ IMU_alg.ino
@@ -3,21 +3,9 @@
 #include <Adafruit_LSM9DS0.h>
 #include <Adafruit_Sensor.h>  // not used in this demo but required!
 
-/*  Flight stages:
-                           *  0: pre-launch
-                           *  1: engine-burn
-                           *  2: early-coast-phase
-                           *  3: late-coast-phase
-                           *  4: descent
-*/
 
-// i2c
+//i2c
 Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0();
-
-// You can also use software SPI
-//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(13, 12, 11, 10, 9);
-// Or hardware SPI! In this case, only CS pins are passed in
-//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(10, 9);
 
 void setupSensor()
 {
@@ -40,11 +28,21 @@ void setupSensor()
   //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
 }
 
-void setup() 
-{
-#ifndef ESP8266
-  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
-#endif
+
+
+void setup() {
+
+  setupCheck();
+  datalogLegend();
+
+}
+
+
+void setupCheck(){
+  #ifndef ESP8266
+    while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+  #endif
+  
   Serial.begin(9600);
   Serial.println("LSM raw read demo");
   
@@ -57,15 +55,45 @@ void setup()
   Serial.println("Found LSM9DS0 9DOF");
   Serial.println("");
   Serial.println("");
-}
 
+  }
+
+
+//printing out legend for values that will be printed for data logging
+void datalogLegend(){
+  Serial.print("Time");
+  Serial.print(", ");
+  Serial.print("z-accel");
+  Serial.print(", ");
+  Serial.print("Flight Stage");
+  Serial.print(", ");
+  Serial.print("max drag");
+  Serial.print(", ");
+  Serial.print("x-accel");
+  Serial.print(", ");
+  Serial.print("y-accel");
+  Serial.println();
+  }
+
+
+
+//variables
 int flightStage = 0;
-boolean activate = false;
-boolean safetyEnvelope = true;
-double maxDrag;
+/*  Flight stages:
+ *  0: pre-launch
+ *  1: engine-burn
+ *  2: early-coast-phase
+ *  3: late-coast-phase
+ *  4: descent
+*/
+double maxDrag = 0;
+boolean isSafe = true;
 
-void loop() 
-{
+//constants
+const float noiseBuffer = 0.5; //arbitrary value for now
+
+
+void loop() {
   lsm.read();
 
   sensors_event_t accel, mag, gyro, temp;
@@ -75,55 +103,89 @@ void loop()
   int xAccel = accel.acceleration.x;
   int yAccel = accel.acceleration.y;
   int zAccel = accel.acceleration.z;
-
-  int xMax = 5000; //arbitrary number for now
-  int yMax = 5000; 
-
   
-  Serial.print(xAccel); Serial.print(", ");
-  Serial.print(yAccel); Serial.print(", ");
-  Serial.print(zAccel);
-  delay(50);
-
-  //updating the maximum drag value all flight
-  if(zAccel > maxDrag){
-    maxDrag = zAccel;
-    }
-
-  //Safety Envelope
-  if(xAccel >= xMax || yAccel >= yMax){
-    safetyEnvelope = false;
-    } 
-
-   //maybe I should change this to <=-12 or something because during burn the IMU will feel multiple g's
-   //using near() function you can create a buffer
-   if(zAccel == -9.81){ //need to include noise buffer
-      flightStage == 0; //pre-launch, do not activate at all
-    } else if(zAccel < -9.81){
-      flightStage == 1; // burn
-    } else if(zAccel >= 0){ 
-      flightStage == 2; //early-coast, this is when we want to activate the plasma
-    } else if(zAccel <= maxDrag/2 && zAccel != 0){
-      flightStage == 3; //late-coast, if plasma isn't activated yet then activate it now
-    } else if(zAccel == 0){
-      flightStage == 4; //descent, do not activate at all
-    }
-
-    //to differentiate between early and late coast: after burn, save the acceleration value from the IMU
-    //that acceleration value is the maximum drag that the rocket will feel during flight
-
-    //boolean switch to tell actuator to turn on
-    if(flightStage == 2 || flightStage == 3 && safetyEnvelope){ 
-      activate = true;
-      }
-
-    //printing flight stages for testing
-    Serial.print("     Flight Stage: ");
-    Serial.print(flightStage);
-    if(activate){
-      Serial.print("... ACTIVATED");
-    }
-    Serial.println();
+  delay(50); //this delay value can be changed, but might lead to more/less noise
+  updateMaxDrag(zAccel);
+  flightStage = flightStageDecision(zAccel);
+  printData(zAccel, flightStage, maxDrag, xAccel, yAccel);
 
   
 }
+
+
+//updating the maximum drag value all flight
+void updateMaxDrag(float zAccel){
+
+  if(zAccel > maxDrag){
+    maxDrag = zAccel;
+    }
+  
+  }
+
+
+//very cursory attempt at the safety cone algorithm
+//dont worry about this
+void safetyCone(float xAccel, float yAccel){
+  int xMax = 5000; //arbitrary number for now
+  int yMax = 5000; 
+
+  if(xAccel >= xMax || yAccel >= yMax){
+    isSafe = false;
+    } 
+
+  }
+
+
+
+//not used in final program so just ignore
+void plotReadings(float xAccel, float yAccel, float zAccel){
+  //these print values are for testing/troubleshooting
+  Serial.print(xAccel); Serial.print(", ");
+  Serial.print(yAccel); Serial.print(", ");
+  Serial.print(zAccel);
+  }
+
+
+
+//the main part of the program
+//this algorithm determines the rocket's flight stage based on the z-acceleration reading from the IMU
+float flightStageDecision(float zAccel){
+  
+    if(zAccel >= -9.81 - noiseBuffer && zAccel <= -9.81 + noiseBuffer){ //force of gravity //buffer is included due to possibly noise in LSM readings 
+      flightStage = 0; //pre-launch, do not activate at all
+    } else if(zAccel < -9.81){ // force of gravity plus engine thrust
+      flightStage = 1; // burn
+    } else if(zAccel >= 0 && zAccel > maxDrag/2){  //drag
+      flightStage = 2; //early-coast, this is when we want to activate the plasma
+    } else if(zAccel <= maxDrag/2 && zAccel != 0){ //drag, but less drag than before
+      flightStage = 3; //late-coast, if plasma isn't activated yet then activate it now
+    } else if(zAccel >= -noiseBuffer && zAccel <= noiseBuffer){ //feels zero Gs in free fall; same noiseBuffer as pre-launch, 
+                                                                //maybe the buffer value should be different?
+      flightStage = 4; //descent, do not activate at all
+    }
+    //to differentiate between early and late coast: after burn, save the acceleration value from the IMU
+    //that acceleration value is the maximum drag that the rocket will feel during flight
+
+    return flightStage;
+ 
+  }
+  
+
+//output for data logging ... CSV format
+//time, zAccel, flightStage, maxDrag, xAccel, yAccel
+void printData(float zAccel, float flightStage, float maxDrag, float xAccel, float yAccel){
+
+  Serial.print(millis());
+  Serial.print(", ");
+  Serial.print(zAccel);
+  Serial.print(", ");
+  Serial.print(flightStage);
+  Serial.print(", ");
+  Serial.print(maxDrag);
+  Serial.print(", ");
+  Serial.print(xAccel);
+  Serial.print(", ");
+  Serial.print(yAccel);
+  Serial.println();
+  
+  }
