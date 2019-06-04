65a60468dd66cc96db972b51f5ccd0d49bc69678
mowalker1 <51222092+mowalker1@users.noreply.github.com>
2019-06-03 18:29:09 -0700

    New temp sensor code (untested)

---
+++ Flux_Sensor_Test/Temp_Code_Current.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+#include <SparkFunTMP102.h>
+#include<SPI.h>
+#include<SD.h>
+
+//SCL to A5
+//SDA to A4
+//GND to GND
+//1.4 to 3.3V
+
+const int chipSelect = 4;
+String file_name = "tempsens.txt";
+unsigned long startTime = 0;
+ 
+TMP102 tempSens(0x48);
+
+void setup() {
+  tempSens.begin();
+  Serial.begin(9600);
+}
+
+void loop() { 
+  float Tempurature;
+  Tempurature = tempSens.readTempF();
+  Serial.println(Tempurature);
+  
+  File tempFile = SD.open(file_name, FILE_WRITE);
+  if (tempFile) {
+    tempFile.print(millis() - startTime);
+    tempFile.print(",");
+    tempFile.print(Temperature);
+    tempFile.println();
+    tempFile.close();
+  } else {
+    Serial.println("problem writing");
+  }
+   delay(400);
+
+}
+
+void setupSD() {
+  Serial.print("Initializing SD card..");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed");
+    while(1);
+  }
+  Serial.println("Card initialized");
+  startTime = millis();
+}
+
+void cma(File f) {
+  f.print(",");
+}
